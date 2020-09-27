a279e5582d9245b635e33e785217c7201335a933
Aakash <subs@appsparkler.com>
2020-09-27 12:47:30 +0530

    working madge-tool

---
+++ spec/bjtps0957h-wordcount-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Bjtps0957hWordcountView from '../lib/bjtps0957h-wordcount-view';
+
+describe('Bjtps0957hWordcountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
